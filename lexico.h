bd8ff8ed6330506d870c7947e5970ca5137da2f7
LuGorex <115434330+LuGorex@users.noreply.github.com>
2025-02-10 11:36:28 -0600

    lexico.h

--- lexico.h
+++ lexico.h
@@ -9,12 +9,30 @@ class TipoSimbolo{
 public:
  static const int ERROR= -1;
  static const int IDENTIFICADOR= 0;
- static const int OPADIC= 1;
- static const int OPMULT= 2;
- static const int PESOS= 3;
- static const int ENTERO= 4;
+ static const int ENTERO= 1;
+ static const int REAL=2;
+ static const int CADENA= 3;
+ static const int TIPO= 4;
+ static const int OPADIC= 5;
+ static const int OPMULT= 6;
+ static const int OPRELAC=7;
+ static const int OPOR =8;
+ static const int OPAND =9;
+ static const int OPNOT =10;
+ static const int OPIGUALDAD =11;
+ static const int PUNTOYCOMA =12;
+ static const int COMA =13;
+ static const int PARENBAI =14;
+ static const int PARENCER =15;
+ static const int CORCHETEABI =16;
+ static const int CORCHETECER =17;
+ static const int IGUAL =18;
+ static const int IF =19;
+ static const int WHILE =20;
+ static const int RETURN =21;
+ static const int ELSE =22;
+ static const int PESOS= 23;
 };
-
 
 class Lexico{
       private:
